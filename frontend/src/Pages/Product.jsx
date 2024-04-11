0ff5fbe6a1762a099ded04abc517e0d011e6129c
sachin <rishabhgit1704@gmail.comin>
2024-04-11 18:16:09 +0530

    changed productdisplay, product, shopcategory

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -2,9 +2,11 @@ import React, { useContext } from 'react'
 import {useParams} from 'react-router-dom';
 import {ShopContext} from '../Context/ShopContext'
 import Breadcrum from '../Components/Breadcrums/Breadcrum';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 // import Footer from '../Components/Footer/Footer';
 
 const Product = () => {
+
     const {all_product} =  useContext(ShopContext);
     const {productId} = useParams();
     const product = all_product.find((e)=> e.id === Number(productId));
@@ -12,6 +14,7 @@ const Product = () => {
         <>
     <div>
     <Breadcrum product = {product}/>
+    <ProductDisplay product = {product}/>
     </div>
     </>
     )
