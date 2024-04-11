8d1fcec2839a97f33648ffb145154013af918802
sachin <rishabhgit1704@gmail.comin>
2024-04-11 18:45:21 +0530

    wrapped product id in Product Route

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -7,9 +7,11 @@ import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 const Product = () => {
 
-    const {all_product} =  useContext(ShopContext);
-    const {productId} = useParams();
-    const product = all_product.find((e)=> e.id === Number(productId));
+   const {all_product} = useContext(ShopContext);
+   const {productId} = useParams();
+   const product = all_product.find((e)=> e.id === Number(productId));
+
+
     return (
         <>
     <div>
