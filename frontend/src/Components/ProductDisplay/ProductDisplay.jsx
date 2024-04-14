b7a5af0b903774f5dec559548f3bf7b90b11a6fc
Rishabh2001Parihar <rishabhsinghparihar02@gmail.com>
2024-04-14 12:36:49 +0530

    created a logic for add to cart

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './ProductDisplay.css'
 import star_icon from '../Assets/star_icon.png';
 import star_dull_icon from '../Assets/star_dull_icon.png';
+import { ShopContext } from '../../Context/ShopContext';
 
 
 const ProductDisplay = (props) => {
     const {product} = props;
+const {addToCart} = useContext(ShopContext);
+
   return (
 <div className="productdisplay">
 <div className="productdisaplay-left">
@@ -49,7 +52,7 @@ const ProductDisplay = (props) => {
     </div>
 </div>
 
-<button>Add to Cart</button>
+<button onClick={()=>{addToCart(product.id)}}>Add to Cart</button>
 <p className="productdisplay-right-category">
     <span>Category : </span>Women T-Shirt, Crop Top
 </p>
--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { createContext } from "react";
 import all_product from "../Components/Assets/all_product";
 
@@ -6,9 +6,32 @@ import all_product from "../Components/Assets/all_product";
 export const ShopContext = createContext(null);  //created ShopContext using createContext and assigned it null
 
 
-const ShopContextProvider = (props)=>{                 //created a ShopContextProvider()
-  const contextValue = {all_product};    // a variable is created to store the data and values that 
-                                        // we want to access using context
+const getDefaultCart = ()=>{                                 //This is the logic to create empty cart
+  const cart ={};
+for (let index = 0; index < all_product.length+1; index++) {
+    cart[index]=0;
+  }
+  return cart;
+}
+
+
+
+const ShopContextProvider = (props)=>{     //created a ShopContextProvider()
+
+  const [cartItems,setCartItems] =useState(getDefaultCart()); // use state to get all products data 
+
+const addToCart =(itemId)=>{
+  setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+  console.log(cartItems);
+}
+
+const removeFromCart =(itemId)=>{
+  setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+}
+
+const contextValue = {all_product,cartItems,addToCart,removeFromCart};    // a variable is created to store the data and values that 
+// we want to access using context
+
   return(
     <ShopContext.Provider value={contextValue}>
 {props.children}
