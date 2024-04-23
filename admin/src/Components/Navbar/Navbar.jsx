cb491674f4acd17c692b889a630dc3c2ccac2b41
sachin <rishabhgit1704@gmail.comin>
2024-04-23 18:06:43 +0530

    Added logo image and profile image

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import './Navbar.css'
-import navlogo from '../../assets/banner_kids.png'
-import navProfile from '../../assets/arrow.png'
+import navlogo from '../../assets/Nav_logo.png'
+import navProfile from '../../assets/Nav_Profile.png'
 
 
 const Navbar = () => {
   return (
     <div className="navbar">
-        <h1> This on is navbar</h1>
       <img src={navlogo} alt="" className="nav-logo" />
       <img src={navProfile} alt="" className="nav-profile" />
     </div>
