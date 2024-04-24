579aa73ecbcccc246910de9a34ee9a647fd729ca
sachin <rishabhgit1704@gmail.comin>
2024-04-24 12:17:44 +0530

    Designed the admin panel for the website Initial Commit

---
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './List_Product.css'
+
+const ListProduct = () => {
+  return (
+    <div className="list-product">
+
+    </div>
+  )
+}
+
+export default ListProduct
