6c570028799ef4cd4429c294ab580181380c7a24
Rishabh2001Parihar <rishabhsinghparihar02@gmail.com>
2024-04-27 21:29:40 +0530

    fetched the products from the database

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './List_Product.css'
+import cross_icon from '../../assets/cart_cross_icon.png'
 
 const ListProduct = () => {
+
+const [allproducts, setAllproducts] = useState([]);
+
+const fetchInfo = async()=>{
+  await fetch('http://localhost:4000/allproducts')             //fetched the all products from its path
+  .then((res)=>res.json())                           //converted the data into json format
+  .then((data)=>{setAllproducts(data)});             //saved the data in setallproduct state variable
+}
+
+
+useEffect(()=>{
+  fetchInfo();
+},[])             //this function will executed only once because of []
   return (
     <div className="list-product">
 <h1>All Products Lsit</h1>
@@ -14,7 +28,19 @@ const ListProduct = () => {
   <p>Category</p>
   <p>Remove</p>
 </div>
+<div className="listproduct-allproducts">
+{allproducts.map((product,index)=>{
+    return <div key={index} className="listproduct-format-main listproduct-format">
+      <img src={product.image} alt="" className="listproduct-product-icon" />
+<p>{product.name}</p>    
+<p>${product.old_price}</p>
+<p>${product.new_price}</p>
+<p>{product.category}</p>
+<img className='listproduct-remove-icon' src={cross_icon} alt="" />
 
+    </div>                                  //created a template to map all the products
+})}
+</div>
     </div>
   )
 }
