7fbbcad584069a999b199956cf3a500fef027ff1
sachin <rishabhgit1704@gmail.comin>
2024-04-29 12:27:21 +0530

    Added the Logic to remove Products from Admin Panel and the database

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -16,6 +16,19 @@ const fetchInfo = async()=>{
 useEffect(()=>{
   fetchInfo();
 },[])             //this function will executed only once because of []
+
+
+const remove_product = async (id)=>{
+  await fetch('http://localhost:4000/removeproduct',{
+      method:'POST',
+      headers:{
+        Accept:'application/json',
+        'Content-Type':'application/json',
+      },
+      body:JSON.stringify({id:id})
+    })
+    await fetchInfo();
+}
   return (
     <div className="list-product">
 <h1>All Products Lsit</h1>
@@ -36,7 +49,7 @@ useEffect(()=>{
 <p>${product.old_price}</p>
 <p>${product.new_price}</p>
 <p>{product.category}</p>
-<img className='listproduct-remove-icon' src={cross_icon} alt="" />
+<img onClick={()=>{remove_product(product.id)}} className='listproduct-remove-icon' src={cross_icon} alt="" />
 
     </div> 
     <hr />  
