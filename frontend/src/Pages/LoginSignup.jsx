6b01905fe9edf0e230d7c836a59369da82845767
sachin <rishabhgit1704@gmail.comin>
2024-04-30 15:47:22 +0530

    Completed the Login and signup Authentication

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -18,12 +18,50 @@ export default function LoginSignup() {
     setFormData({...formData,[e.target.name]:e.target.value})
   }
 
+
+  
   const login = async () =>{
 console.log("login",formData);
+
+let responseData;
+    await fetch('http://localhost:4000/login',{
+      method:'POST',
+      headers:{
+        Accept:'application/form-data',
+        'Content-Type':'application/json',
+      },
+      body:JSON.stringify(formData),
+    }).then((reponse)=>reponse.json()).then((data)=>responseData=data)
+
+    if(responseData.success){
+      localStorage.setItem('auth-token',responseData.token);
+      window.location.replace("/");
+    }
+    else{
+      alert(responseData.errors)
+    }
+
   }
 
   const signup = async () =>{
     console.log("signup",formData);
+    let responseData;
+    await fetch('http://localhost:4000/signup',{
+      method:'POST',
+      headers:{
+        Accept:'application/form-data',
+        'Content-Type':'application/json',
+      },
+      body:JSON.stringify(formData),
+    }).then((reponse)=>reponse.json()).then((data)=>responseData=data)
+
+    if(responseData.success){
+      localStorage.setItem('auth-token',responseData.token);
+      window.location.replace("/");
+    }
+    else{
+      alert(responseData.errors)
+    }
   }
 
 
