2617bf40efae91cd5e62a5998cb2ce82fc67cc73
sachin <rishabhgit1704@gmail.comin>
2024-05-02 18:38:57 +0530

    Added the styling to home page

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Hero.css'
+import backgroundImage from '/home/rishabh/React-Project/frontend/src/Components/Assets/denim-2_1400x.webp';
 
 const Hero = () => {
     return (
@@ -8,8 +9,7 @@ const Hero = () => {
                 <div
                     className="single-slider slider-height d-flex align-items-center justify-content-center"
                     style={{
-                        backgroundImage:
-                            "url(https://wphix.com/template/sikkha-prv/sikkha/img/slider/slider_bg_1.jpg)"
+                        backgroundImage:`url(${backgroundImage})`,
                     }}
                 >
                     <div className="container">
@@ -21,21 +21,21 @@ const Hero = () => {
                                         data-animation="fadeInUp"
                                         data-delay=".2s"
                                     >
-                                        <span>Admissions Going On </span>
+                                        {/* <span>Admissions Going On </span> */}
                                         <br />
-                                        Eduara University
+                                        {/* Eduara University */}
                                     </h1>
-                                    <p data-animation="fadeInUp" data-delay=".4s">
+                                    {/* <p data-animation="fadeInUp" data-delay=".4s">
                                         Sorem ipsum dolor sit amet consectetur adipisicing elit sed do
                                         eiusmod tempor <br /> incididunt ut labore et dolore magna
                                         aliqua enim ad minime.
-                                    </p>
+                                    </p> */}
                                     <button
                                         className="theme-btn"
                                         data-animation="fadeInUp"
                                         data-delay=".6s"
                                     >
-                                        <span className="btn-text">admit now</span>
+                                        {/* <span className="btn-text">admit now</span> */}
                                     </button>
                                 </div>
                             </div>
