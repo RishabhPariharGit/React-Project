78bb112a28003ec8a30bdc6f9e5af70aa8faf3e7
sachin <rishabhgit1704@gmail.comin>
2024-05-14 18:30:52 +0530

    added the logic for add to cart

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Item.css'
 import { Link } from 'react-router-dom'
+import { ShopContext } from '../../Context/ShopContext';
+
 
 const Item = (props) => {
+
+
+const {product} = props;
+const {addToCart} = useContext(ShopContext);
+
     return (
-        <div className="item">
+        <div className="item" style={{textdecoration:'none'}}>
            <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0,0)} src={props.image} alt='' /></Link> 
-            <p>{props.name}</p>
+           <Link to={`/product/${props.id}`} ><p>{props.name}</p></Link> 
+
+
+            <button onClick={()=>{addToCart(props.id)}}>Add to Cart</button>
+
+
             <div className="item-prices">
                 <div className="item-price-new">
                     ${props.new_price}
