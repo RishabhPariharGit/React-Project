927fe081e3446d004498f332654b26ee0c14cc93
sachin <rishabhgit1704@gmail.comin>
2024-05-16 18:26:56 +0530

    added the styling to offers section

--- frontend/src/Components/Offers/Offers.jsx
+++ frontend/src/Components/Offers/Offers.jsx
@@ -4,8 +4,8 @@ import './Offers.css'
 export default function Offers() {
   return (
     <div className="offers_cont">
-  <h1>LET'S GO CAMPING</h1>
-  <p> Are you ready for the adventure?</p>
+  <h1>Shop The Best !</h1>
+  <p> Checkout the latest collections.</p>
   <button class="firstbutton">Join now</button> 
   {/* <button class="secondbutton">Learn more</button>  */}
   </div>
